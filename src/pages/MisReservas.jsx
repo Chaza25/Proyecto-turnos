021767d4a5edb1c5dee0f403cd9c81e5e5284f04
Agustin Chazarreta Cruz <aguschacruz@gmail.com>
2025-08-27 13:15:51 -0300

    Agregamos cards para mostrar las reservas, actualizacion de login

--- src/pages/MisReservas.jsx
+++ src/pages/MisReservas.jsx
@@ -1,27 +1,26 @@
 import useReservaStore from "../../store/reservaStore";
+import ReservaCard from "../components/ReservaCard";
 
 function MisReservas() {
-    const {
-        reservasConfirmadas,
-    } = useReservaStore();
 
-    const { servicio, fecha, horario, cantidadPersonas, observaciones } = reservasConfirmadas;
+    const reservasConfirmadas = useReservaStore((state) => state.reservasConfirmadas);
 
     return(
-        <>
-        <h1 className="text-8xl flex justify-center mx-auto font-extrabold text-center">Mis Reservas</h1>
-        {reservasConfirmadas.map((reserva, index) => (
-            <div key={index} className="max-w-lg mx-auto bg-white shadow-lg rounded-lg p-6 my-6">
-                <h2 className="text-2xl font-bold mb-4 text-center">Reserva {index + 1}</h2>
-                <p><span className="font-semibold">Servicio:</span> {servicio}</p>
-                <p><span className="font-semibold">Fecha:</span> {fecha}</p>
-                <p><span className="font-semibold">Horario:</span> {horario}</p>
-                {cantidadPersonas && <p><span className="font-semibold">Cantidad de personas:</span> {cantidadPersonas}</p>}
-                {observaciones && <p><span className="font-semibold">Observaciones:</span> {observaciones}</p>}
+        <div className="px-6 py-10">
+            <h1 className="text-3xl font-bold text-center">Mis reservas</h1>
+            <div className="mt-6 space-y-6">
+                {reservasConfirmadas.length === 0 ? (
+                    <p className="text-center text-lg text-gray-500 mt-6 pt-30">No tienes reservas confirmadas</p>
+                ) : (
+                    reservasConfirmadas.map((reserva, index) => (
+                        <ReservaCard
+                            key={index}
+                            {...reserva}
+                        />
+                    ))
+                )}
             </div>
-        )
-        )}
-        </>
+        </div>
     )
 }
 
