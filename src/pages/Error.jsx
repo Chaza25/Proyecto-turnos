6942e807b009d45e2d11f72766bccebd997aadf7
Agustin Chazarreta Cruz <aguschacruz@gmail.com>
2025-09-12 20:28:50 -0300

    Eliminar archivos duplicados en raiz

--- src/pages/Error.jsx
+++
@@ -1,9 +0,0 @@
-function Error() {
-    return(
-        <>
-        <h1 className="text-8xl flex justify-center mx-auto font-extrabold text-center">Pagina de error</h1>
-        </>
-    )
-}
-
-export default Error;
